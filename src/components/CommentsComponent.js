a93c6b169309274753b977b4652a5f9e365f7df2
AHMINA Marouan <AhminaMar1@gmail.com>
2020-10-31 00:04:45 +0100

    Add the first post - The opening

--- src/components/CommentsComponent.js
+++ src/components/CommentsComponent.js
@@ -403,7 +403,7 @@ const CommentsComponent = ({id_post, token_props, setToken_props}) => {
                                             
                                             {(editCommentId===el.id)
                                             ?<EditCommentForm originalComment={el.comment} comment_id={el.id} token={token_props} setTokenError={setTokenError} comments={comments} setComments={setComments} showEditFormComment={showEditFormComment} setStatus={setStatus} styleEntryOrExit={styleEditFormCommentEntryOrExit} styleFormCommentEntry={styleFormCommentEntry} styleFormCommentExit={styleFormCommentExit} placeholderComment="أكتب التعديل المراد"/>
-                                            :<p>{el.comment}</p>}
+                                            :<pre>{el.comment}</pre>}
                                             
 
                                             <div className="comment_element_more">
